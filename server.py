517a6e561fdb2941a4545b70853586ca3bf4dd75
Lukas Kurz <vb.net@kurzweb.de>
2021-08-10 18:45:21 +0200

    Prepare api endpoint in server

--- server.py
+++ server.py
@@ -1,9 +1,19 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse
+import json
+
 
 class ServerHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         path = urlparse(self.path).path
+
+        if path == '/api':
+            content = bytes(f"API", 'utf-8')
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(content)
+            return
+
         if path == '/':
             path = '/index.html'
         path = f"content\{path[1:]}"
@@ -23,6 +33,7 @@ class ServerHandler(BaseHTTPRequestHandler):
         query = urlparse(self.path).query
         return dict(qc.split("=") for qc in query.split("&"))
     '''
+
 
 httpd = HTTPServer(('localhost', 8080), ServerHandler)
 httpd.serve_forever()
